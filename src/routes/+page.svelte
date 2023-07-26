ad9925954f24e68c4f83ff7fa172f4ea4107bb20
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2023-07-25 21:50:53 -0700

    Add Modal to Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,6 +25,23 @@
 	let defaultModal = false;
 </script>
 
+<Modal title="Terms of Service" bind:open={defaultModal} autoclose>
+	<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+		With less than a month to go before the European Union enacts new consumer privacy laws for its
+		citizens, companies around the world are updating their terms of service agreements to comply.
+	</p>
+	<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+		The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
+		and is meant to ensure a common set of data rights in the European Union. It requires
+		organizations to notify users as soon as possible of high-risk data breaches that could
+		personally affect them.
+	</p>
+	<svelte:fragment slot="footer">
+		<Button on:click={() => alert('Handle "success"')}>I accept</Button>
+		<Button color="alternative">Decline</Button>
+	</svelte:fragment>
+</Modal>
+
 <div class="m-auto max-w-screen-xl w-full">
 	<header class="w-full relative">
 		<Navbar
@@ -247,7 +264,9 @@
 							>
 						</Input>
 
-						<Button color="primary" size="xs">Subscribe</Button>
+						<Button color="primary" size="xs" on:click={() => (defaultModal = true)}
+							>Subscribe</Button
+						>
 					</ButtonGroup>
 				</div>
 			</div>
