abcdd5caaf89d894ae81e9a4a849175538aa997c
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2023-07-27 14:57:54 -0700

    Add Tailwind Typography plugin and update content on 'About' page.
    Added the Typography plugin to tailwind.config.cjs and included its dependency in package.json and pnpm-lock.yaml files for better text formatting and styling. Also updated content in src/routes/about/+page.svelte, replacing the placeholder content with relevant articles. The Typography plugin will help to enhance the readability and overall aesthetic of the text on the website.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,32 @@
-<script lang="ts">
-	import type { PageData } from './$types';
+<article class="prose prose-sm sm:prose-base md:prose-xl lg:prose-2xl">
+    <h1>Garlic bread with cheese: What the science tells us</h1>
+    <p>
+        For years parents have espoused the health benefits of eating garlic bread with cheese to their
+        children, with the food earning such an iconic status in our culture that kids will often dress
+        up as warm, cheesy loaf for Halloween.
+    </p>
+    <p>
+        But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+        springing up around the country.
+    </p>
+</article>
 
-	export let data: PageData;
-</script>
+<br />
+<hr />
+<br />
 
-<h1>Coming soon...</h1>
+<article class="prose prose-sm sm:prose-base md:prose-xl lg:prose-2xl">
+    <h1>Prototyping from A to Z: best practices for successful prototypes</h1>
+    <p class="lead">Flowbite is an open-source library of UI components built with the utility-first classes from Tailwind CSS. It also includes interactive elements such as dropdowns, modals, datepickers.</p>
+    <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way, you can think things through before committing to an actual design project.</p>
+    <p>But then I found a <a href="/">component library based on Tailwind CSS called Flowbite</a>. It comes with the most commonly used UI components, such as buttons, navigation bars, cards, form elements, and more which are conveniently built with the utility classes from Tailwind CSS.</p>
+
+    <h2>When does design come in handy?</h2>
+    <p>While it might seem like extra work at a first glance, here are some key moments in which prototyping will come in handy:</p>
+    <ol>
+        <li><strong>Usability testing</strong>. Does your user know how to exit out of screens? Can they follow your intended user journey and buy something from the site you’ve designed? By running a usability test, you’ll be able to see how users will interact with your design once it’s live;</li>
+        <li><strong>Involving stakeholders</strong>. Need to check if your GDPR consent boxes are displaying properly? Pass your prototype to your data protection team and they can test it for real;</li>
+        <li><strong>Impressing a client</strong>. Prototypes can help explain or even sell your idea by providing your client with a hands-on experience;</li>
+        <li><strong>Communicating your vision</strong>. By using an interactive medium to preview and test design elements, designers and developers can understand each other — and the project — better.</li>
+    </ol>
+</article>
